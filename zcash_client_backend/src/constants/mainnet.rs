62f98346fa1f68e991aaba2ddb1e349be93255c4
Arjun <denverbdr@protonmail.com>
2019-11-04 09:47:53 -0800

    Update cointype

--- zcash_client_backend/src/constants/mainnet.rs
+++ zcash_client_backend/src/constants/mainnet.rs
@@ -1,7 +1,7 @@
 /// The mainnet coin type for ZEC, as defined by [SLIP 44].
 ///
 /// [SLIP 44]: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-pub const COIN_TYPE: u32 = 133;
+pub const COIN_TYPE: u32 = 347;
 
 /// The HRP for a Bech32-encoded mainnet [`ExtendedSpendingKey`].
 ///
